77e4e8ab097b23469731597742dadbc2788bbed3
xfortech <shiyasnehma2@gmail.com>
2025-01-29 17:19:08 +0530

    empty component

--- resources/views/components/no-data-component.blade.php
+++ resources/views/components/no-data-component.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="empty w-100 d-flex justify-content-center align-items-center flex-column h-100 ">
-        <div class="empty-img"><img
-                src="{{ asset('/assets/img/nodata.png') }}" height="128"
+    <div class="empty w-full flex-col flex justify-center items-center h-[calc(100vh-10rem)] ">
+        <div class="empty-img h-40"><img
+                src="{{ asset('/assets/img/nodata.png') }}" class="w-full h-full"
                 alt="">
         </div>
         <p class="empty-title">{{ $message}}</p>
