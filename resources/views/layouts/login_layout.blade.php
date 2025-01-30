9da36e050dced31589fc9136f143ddbe27b04f5a
ayishunizar <112337730+ayishunizar@users.noreply.github.com>
2025-01-30 17:52:21 +0530

    login

--- resources/views/layouts/login_layout.blade.php
+++ resources/views/layouts/login_layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,16 +9,86 @@
     <meta name="author" content="SketchStack">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-    <link rel="canonical" href="index.html" />
-    <title>{{getStoreName()}}</title>
-    <link class="js-stylesheet" href="{{ asset('/assets/css/light.css') }}" rel="stylesheet">
-    <link href="{{ asset('/assets/css/style.css') }}" rel="stylesheet">
+    <title>{{ getStoreName() }}</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap"
+        rel="stylesheet" />
+    <link href="{{ asset('new_assets/vendors/keenicons/styles.bundle.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('new_assets/css/toastr.min.css') }}">
+
+    <link href="{{ asset('new_assets/css/styles.css') }}" rel="stylesheet" />
+    <script>
+        const defaultThemeMode = 'light'; // light|dark|system
+        document.documentElement.classList.add(defaultThemeMode);
+    </script>
+    @stack('css')
     @livewireScripts()
     @livewireStyles()
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default" class="login-bg">
-    {{$slot}}
+
+<body
+    class="antialiased flex h-full w-screen text-base text-gray-700 [--tw-page-bg:#f6f6f6] [--tw-page-bg-dark:var(--tw-coal-200)] [--tw-content-bg:var(--tw-light)] [--tw-content-bg-dark:var(--tw-coal-500)] [--tw-content-scrollbar-color:#e8e8e8] [--tw-header-height:58px] [--tw-sidebar-width:58px] [--tw-navbar-height:56px] bg-[--tw-page-bg] dark:bg-[--tw-page-bg-dark] lg:overflow-hidden">
+
+
+    
+        <div
+            class=" w-full flex flex-col lg:[scrollbar-width:auto] lg:light:[--tw-scrollbar-thumb-color:var(--tw-content-scrollbar-color)] h-[calc(100vh-2rem)] overflow-y-auto">
+            {{ $slot }}
+        </div>
+    
+    
+    <script src="{{ asset('new_assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('new_assets/js/jquery-3.7.1.min.js') }}"></script> <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+    <script src="{{ asset('new_assets/js/toastr.min.js') }}"></script>
+    <script src="{{ asset('new_assets/js/core.bundle.js') }}"></script>
+    <script src="{{ asset('new_assets/js/widgets/general.js') }}"></script>
+
+    <script>
+        "use strict";
+        Livewire.on('closemodal', (event) => {
+            let modals = document.querySelectorAll('.modal.open')
+            for (let i = 0; i < modals.length; i++) {
+                let modal = window.KTModal.getOrCreateInstance(modals[i])
+                modal.hide()
+            }
+        });
+    </script>
+    <script>
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('notify', (event) => {
+                toastr[event[0].type](event[0].message, event[0].title)
+            });
+        });
+    </script>
+    <!-- <script>
+        "use strict"
+        Livewire.on('closemodal', () => {
+            $('.modal').modal('hide');
+            $('.modal-backdrop').remove();
+            $('body').removeClass('modal-open');
+            $('body').removeAttr('style');
+        })
+    </script>
+    <script>
+        "use strict";
+        Livewire.on('reloadpage', () => {
+            window.location.reload();
+        })
+    </script>
+    <script>
+        "use strict";
+        window.addEventListener('alert', event => {
+            toastr[event.detail.type](event.detail.message, event.detail.title ?? '');
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true,
+            }
+        });
+        @if (Session::has('message'))
+            toastr.info("{{ Session::get('message') }}");
+        @endif
+    </script> -->
+    @stack('script')
 </body>
-<script src="{{ asset('/assets/js/app.js') }}"></script>
+
 </html>
